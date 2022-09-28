f704694da63c1941f8891fd282ee0498653f0c19
meraj2085 <samirmeraj60@gmail.com>
2022-09-28 21:08:50 +0600

    adding components

---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Exercises.css'
+const Exercises = () => {
+     return (
+          <div>
+               <p>From Exersise</p>
+          </div>
+     );
+};
+
+export default Exercises;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Exercises from '../Exercises/Exercises';
+import './Home.css'
+const Home = () => {
+     return (
+          <div>
+               <h1>From Home</h1>
+               <Exercises></Exercises>
+          </div>
+     );
+};
+
+export default Home;
