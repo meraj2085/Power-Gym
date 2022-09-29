eb9d218041bf18948e77de12c4d662ee646d6542
meraj2085 <samirmeraj60@gmail.com>
2022-09-29 09:43:32 +0600

    adding margin top on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css'
 const Navbar = () => {
      return (
           <div>
-               <h1 className='header-text'><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>Power <span>Gym</span></h1>
+               <h1 className='header-text my-2'><FontAwesomeIcon icon={faDumbbell}></FontAwesomeIcon>Power <span>Gym</span></h1>
           </div>
      );
 };
