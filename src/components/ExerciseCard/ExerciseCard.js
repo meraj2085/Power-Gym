6fa758ea56f335afe669b2a3800d2b139b71c7f6
meraj2085 <samirmeraj60@gmail.com>
2022-09-29 09:00:21 +0600

    making page responsive

--- src/components/ExerciseCard/ExerciseCard.js
+++ src/components/ExerciseCard/ExerciseCard.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './ExerciseCard.css'
+import React from "react";
+import "./ExerciseCard.css";
 
-const ExerciseCard = ({exercise, activity, setActivity}) => {
-     const {id, image, name, timeRequired} = exercise;
+const ExerciseCard = ({ exercise, activity, setActivity }) => {
+  const { id, image, name, timeRequired } = exercise;
 
-     const handleExerciseTime = () =>{
-          setActivity(activity + timeRequired)
-     }
-     return (
-          <div>
-               <div className='exercise-card'>
+  const handleExerciseTime = () => {
+    setActivity(activity + timeRequired);
+  };
+  return (
+    <div>
+      {/* <div className='exercise-card'>
                <img src={image} alt="" />
                <div className='exercise-details'>
                     <h6>{name}</h6>
@@ -18,9 +18,21 @@ const ExerciseCard = ({exercise, activity, setActivity}) => {
                <div onClick={handleExerciseTime} className='exercise-btn-container'>
                     <button className='exercise-btn'>Add to list</button>
                </div>
-               </div>
+               </div> */}
+      <div class="card" style={{ minWidth: "18rem" }}>
+        <img src={image} className="card-img-top" alt="" />
+        <div class="card-body">
+          <div className="exercise-details">
+            <h6>{name}</h6>
+            <p>Time required : {timeRequired}m</p>
           </div>
-     );
+          <div onClick={handleExerciseTime} className="exercise-btn-container">
+            <button className="exercise-btn">Add to list</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExerciseCard;
