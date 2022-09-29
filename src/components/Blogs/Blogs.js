3edc3abc7bf9da29c691e44b2be74ae048c310df
meraj2085 <samirmeraj60@gmail.com>
2022-09-29 09:40:44 +0600

    adding blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,35 +3,82 @@ import "./Blogs.css";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1 className="blogs-title">
+    <div className="blogs-container">
+      <h1 classNameName="blogs-title">
         <span style={{ color: "blueviolet" }}>B</span>logs
       </h1>
-      <hr style={{ width: "200px" }} />
-      <div className="questions-container">
-        <div>
-          <p className="question">1: How does react works?</p>
-          <p>
-            Ans: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dignissimos expedita sapiente, culpa sint ullam pariatur inventore
-            molestiae sed eligendi est.
-          </p>
+      <div className="accordion container my-5" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              How does react works?
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>hlw world 3</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p className="question">2: Whats the difference between props & state?</p>
-          <p>
-            Ans: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dignissimos expedita sapiente, culpa sint ullam pariatur inventore
-            molestiae sed eligendi est.
-          </p>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              What's the difference between props & state?
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>hlw world 3</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p className="question">3: On which purposes useEffect() is used except Api data load?</p>
-          <p>
-            Ans: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dignissimos expedita sapiente, culpa sint ullam pariatur inventore
-            molestiae sed eligendi est.
-          </p>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              On which purposes useEffect() is used except Api data load?
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <p>hlw world 3</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
